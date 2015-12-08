19ca5286648e3c6cc8bc3ed6a5b99e89e3f0016b
Patrick Templeton <admin@patricktempleton.xyz>
2015-12-08 17:11:46 -0500

    Moved code into separate files.

--- client/main.js
+++ client/main.js
@@ -1,27 +1,3 @@
-/////////////////////
- //     routing     //
-/////////////////////
-
-Router.configure({
-  layoutTemplate: "ApplicationLayout"
-});
-
-Router.route("/", function(){
-  this.render( "navbar", {to: "navbar"} );
-  this.render( "website_list", {to: "main", path: "/"} );
-});
-
-Router.route("/details/:_id", function(){
-  this.render( "navbar", {to: "navbar"} );
-  this.render( "website_details", {
-    to: "main",
-    path: "details/_id",
-    data: function(){
-      return Websites.findOne({_id: this.params._id});
-    }
-  });
-});
-
   /////////////////////
  //     config      //
 /////////////////////
